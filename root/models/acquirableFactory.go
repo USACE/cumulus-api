d83263d18cd1ee741791b189dc0ad8c5e4ee94dc
brett.palmberg <brett.t.palmberg@usace.army.mil>
2020-05-14 15:31:19 -0700

    Correct SNODAS URL Formatting Error

--- root/models/acquirableFactory.go
+++ root/models/acquirableFactory.go
@@ -25,8 +25,12 @@ type NohrscSnodasUnmaskedAcquirable struct {
 // URLS implements Acquirable interface
 func (a *NohrscSnodasUnmaskedAcquirable) URLS() []string {
 	t := time.Now()
-	urlFmt := "ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/unmasked/2006/01_Jan/SNODAS_unmasked_20060102.tar"
-	return []string{t.Format(urlFmt)}
+	urlBase := "ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/unmasked"
+	urlFmt := "2006/01_Jan/SNODAS_unmasked_20060102.tar"
+
+	url := urlBase + "/" + t.Format(urlFmt)
+
+	return []string{url}
 }
 
 // Info implements Acquirable interface

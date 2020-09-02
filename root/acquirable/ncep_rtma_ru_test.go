e162bae179095a5c4c1a210f8679e52e9b52c60f
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-09-01 23:52:37 -0500

    fix variable name

--- root/acquirable/ncep_rtma_ru_test.go
+++ root/acquirable/ncep_rtma_ru_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRtmaRuURLS(t *testing.T) {
 	dt := time.Date(2020, time.September, 1, 1, 15, 0, 0, time.UTC)
-	urls := RtmaRuURLS("anl", &dt)
+	urls := NcepRtmaRuURLS("anl", &dt)
 	urlsCorrect := []string{
 		"https://nomads.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5_ru.20200901/rtma2p5_ru.t0100z.2dvaranl_ndfd.grb2",
 		"https://nomads.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5_ru.20200901/rtma2p5_ru.t0115z.2dvaranl_ndfd.grb2",

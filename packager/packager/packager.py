af2b2ab78f225287a2ba1fc84a6dfb1373afd671
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-10-23 18:03:34 -0500

    bugfix; missing s3 upload function

--- packager/packager/packager.py
+++ packager/packager/packager.py
@@ -59,6 +59,30 @@ def get_infile(bucket, key, filepath):
             raise
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    Copied from https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return False
+    return True
+
+
 def package(msg, packager_update_fn):
 
     print(json.dumps(msg, indent=2))
@@ -116,9 +140,15 @@ def package(msg, packager_update_fn):
     # Get product count from event contents
     with tempfile.TemporaryDirectory() as td:
         print(f'Working in temporary directory: {td}')
-        outfile = write_contents_to_dssfile(os.path.join(td, os.path.basename(output_key)), basin, contents, callbackFn)
-
         print('Output key is: {}'.format(output_key))
+        outfile = write_contents_to_dssfile(
+            os.path.join(td, os.path.basename(output_key)),
+            basin,
+            contents,
+            callbackFn
+        )
+
+        
 
         # Upload the final output file to S3        
         if CONFIG.CUMULUS_MOCK_S3_UPLOAD:
@@ -128,9 +158,7 @@ def package(msg, packager_update_fn):
             # shutil.copy2 will overwrite a file if it already exists.
             shutil.copy2(outfile, "/output/"+os.path.basename(outfile))
         else:
-            upload_success = upload_file(
-                _outfile, WRITE_TO_BUCKET, output_key
-            )
+            upload_success = upload_file(outfile, CONFIG.WRITE_TO_BUCKET, output_key)
         
         # Call Packager Update Function one last time to add file key to database
         if upload_success:

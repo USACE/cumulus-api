fa386ccd2fde7fe1165b7a2c803f703979b56ff5
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-11-16 14:21:20 -0600

    convenience script; pgadmin4

---
+++ database/pgadmin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Convenience script to start pgadmin4 container on localhost:8080
+docker run -d -p "8080:80" \
+    -e PGADMIN_DEFAULT_EMAIL=postgres@postgres.com \
+    -e PGADMIN_DEFAULT_PASSWORD=postgres \
+    dpage/pgadmin4:4.27

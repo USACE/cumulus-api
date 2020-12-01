5d23edddd6bad7b21b23c31be289eba349d8919d
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-12-01 15:57:35 -0600

    product_group_names

--- database/migrations/feature_product_groups.sql
+++ database/migrations/feature_product_groups.sql
@@ -10,8 +10,8 @@ GRANT SELECT ON product_group TO cumulus_reader;
 GRANT INSERT,UPDATE,DELETE ON product_group TO cumulus_writer;
 -- Product Group Seed Data
 INSERT INTO product_group (id, name) VALUES
-    ('726039da-2f21-4393-a15c-5f6e7ea41b1f', 'PRECIP'),
-    ('d9613031-7cf0-4722-923e-e5c3675a163b', 'AIRTEMP'),
+    ('726039da-2f21-4393-a15c-5f6e7ea41b1f', 'PRECIPITATION'),
+    ('d9613031-7cf0-4722-923e-e5c3675a163b', 'TEMPERATURE'),
     ('57bda84f-ecec-4cd7-b3b1-c0c36f838a05', 'SNOW');
 
 -- Modify Product Table

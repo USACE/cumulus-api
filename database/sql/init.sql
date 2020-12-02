9988516b380d7348ce1d0696e393b92677f09b55
Adam Scarberry <adam.scarberry@usace.army.mil>
2020-12-02 16:59:21 -0500

    shortened path to sql files

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -1,8 +1,8 @@
 \set ON_ERROR_STOP 1
 begin;
 
-\i '/docker-entrypoint-initdb.d/sql/10-tables.sql'
-\i '/docker-entrypoint-initdb.d/sql/20-roles.sql'
-\i '/docker-entrypoint-initdb.d/sql/30-seed_data.sql'
+\i '/sql/10-tables.sql'
+\i '/sql/20-roles.sql'
+\i '/sql/30-seed_data.sql'
 
 commit;

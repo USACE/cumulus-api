d486596c8cef9a53a467739e9df65f175943d1c2
Adam Scarberry <adam.scarberry@usace.army.mil>
2020-12-03 15:07:34 -0500

    fix dpart to 2400

--- root/models/download.go
+++ root/models/download.go
@@ -122,9 +122,11 @@ func BuildPackagerRequest(db *sqlx.DB, d *Download) (*PackagerRequest, error) {
 	sql := `SELECT key,
 	               bucket,
 				   dss_datatype,
-				   dss_cpart,
-	               to_char(datetime_dss_dpart, 'DDMONYYYY:HH24MI') as dss_dpart,
-	               CASE WHEN date_part('hour', datetime_dss_epart) = 0
+				   dss_cpart,				   
+				   CASE WHEN date_part('hour', datetime_dss_dpart) = 0
+	               			THEN to_char(datetime_dss_dpart - interval '1 Day', 'DDMONYYYY:24MI')
+	               	 	ELSE COALESCE(to_char(datetime_dss_dpart, 'DDMONYYYY:HH24MI'), '') END as dss_dpart,				   
+				   CASE WHEN date_part('hour', datetime_dss_epart) = 0
 	               			THEN to_char(datetime_dss_epart - interval '1 Day', 'DDMONYYYY:24MI')
 	               	 	ELSE COALESCE(to_char(datetime_dss_epart, 'DDMONYYYY:HH24MI'), '') END as dss_epart,
 				   dss_fpart,

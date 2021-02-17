2ea00e3673e68a93222b8c433fa317553fb42445
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-02-17 14:55:20 -0600

    _SQS environment variable default

--- async_packager/packager/config.py
+++ async_packager/packager/config.py
@@ -13,9 +13,13 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', default='x')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', default='x')
 AWS_REGION = os.getenv('AWS_REGION', default='us-east-1')
 
-AWS_SECRET_ACCESS_KEY_SQS = os.getenv('AWS_SECRET_ACCESS_KEY_SQS', default='x')
-AWS_ACCESS_KEY_ID_SQS = os.getenv('AWS_ACCESS_KEY_ID_SQS', default='x')
-AWS_REGION_SQS = os.getenv('AWS_REGION_SQS', default='elasticmq')
+# If _SQS versions of AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, AWS_REGION not explicitly set,
+# set to usual environment variables for AWS credentials. Variables with _SQS suffix are intended
+# to be used when override is required for elasticmq (variables set to 'x' or 'elasticmq') for local testing.
+# See Documentation Here: https://github.com/softwaremill/elasticmq#using-the-amazon-boto-python-to-access-an-elasticmq-server
+AWS_SECRET_ACCESS_KEY_SQS = os.getenv('AWS_SECRET_ACCESS_KEY_SQS', default=AWS_SECRET_ACCESS_KEY)
+AWS_ACCESS_KEY_ID_SQS = os.getenv('AWS_ACCESS_KEY_ID_SQS', default=AWS_ACCESS_KEY_ID)
+AWS_REGION_SQS = os.getenv('AWS_REGION_SQS', default=AWS_REGION)
 
 # 
 # Database Credentials

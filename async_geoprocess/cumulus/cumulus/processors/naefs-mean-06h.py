e418feff56b2d7a1695d8bfee2ad634a58c05562
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-06-16 09:30:12 -0500

    Added processor for NAEFS QPF and QTF

--- async_geoprocess/cumulus/cumulus/processors/naefs-mean-06h.py
+++ async_geoprocess/cumulus/cumulus/processors/naefs-mean-06h.py
@@ -14,15 +14,15 @@ def process(infile, outdir):
     outfile_list = list()
 
     # Variables and their product slug
-    grid_variables = {
+    nc_variables = {
         "QPF": "naefs-mean-qpf-06h",
-        # "QTF": "naefs-mean-qtf-06h"
+        "QTF": "naefs-mean-qtf-06h"
     }
     
     # Each band in 'time' is a band in the variable
-    time_ds = gdal.Open(f"NETCDF:{infile}:time")
+    src_time_ds = gdal.Open(f"NETCDF:{infile}:time")
     # Get the since minutes and compute band times
-    time_band = time_ds.GetRasterBand(1)
+    time_band = src_time_ds.GetRasterBand(1)
     time_meta = time_band.GetMetadata_Dict()
     time_unit = time_meta["units"]
     # re pattern matching to get the start date from the 'time' units
@@ -31,14 +31,10 @@ def process(infile, outdir):
     since_time_str = re.search(pattern, time_unit).group(0)
     since_time = datetime.fromisoformat(since_time_str)
     time_array = time_band.ReadAsArray().astype(np.dtype("float32"))
-    date_array = [
-        (lambda x: timedelta(minutes=int(x)) + since_time)(m)
-        for m in time_array[0]
-    ]
-    print(f"Length of date array: {len(date_array)}")
-    
-    for grid_var, grid_slug in grid_variables.items():
-        subdataset = gdal.Open(f"NETCDF:{infile}:{grid_var}")
+
+    min2date = lambda x: timedelta(minutes=int(x)) + since_time
+    for nc_variable, nc_slug in nc_variables.items():
+        subdataset = gdal.Open(f"NETCDF:{infile}:{nc_variable}")
         ds_meta = subdataset.GetMetadata_Dict()
         date_created_str = [
             re.search(pattern, v).group(0)
@@ -46,19 +42,19 @@ def process(infile, outdir):
             if "date_created" in k
         ]
         date_created = datetime.fromisoformat(date_created_str[0])
-        print(f"{date_created=}")
+
         no_bands = subdataset.RasterCount
-        print(f"RasterCount: {no_bands}")
-        xsize = subdataset.RasterXSize
-        ysize = subdataset.RasterYSize
+
         geo_transform = subdataset.GetGeoTransform()
         src_projection = subdataset.GetProjection()
-        # src_spacial_ref = subdataset.GetSpatialRef()
+
         for b in range(1, no_bands):
-            band_date = date_array[b]
-            band_filename = "NAEFSmean_" + grid_var + band_date.strftime("%Y%m%d%H%M") + ".tif"
+            band_date = min2date(time_array[0][b])
+            band_filename = "NAEFSmean_" + nc_variable + band_date.strftime("%Y%m%d%H%M") + ".tif"
             # Get the band and process to raster
             band = subdataset.GetRasterBand(b)
+            xsize = band.XSize
+            ysize = band.YSize
             datatype = band.DataType
             nodata_value = band.GetNoDataValue()
             b_array = band.ReadAsArray(0, 0 , xsize, ysize).astype(np.dtype("float32"))
@@ -86,7 +82,7 @@ def process(infile, outdir):
 
             outfile_list.append(
                 { 
-                    "filetype": grid_slug,
+                    "filetype": nc_slug,
                     "file": cog,
                     "datetime": band_date.isoformat(),
                     "version": date_created.isoformat()

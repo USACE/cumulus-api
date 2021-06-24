4bccc1ee3a8bc72e0d3d8da4b4b803e20daf31b5
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-06-23 21:44:13 -0500

    remove htmlextra reporter; test results will display in GH Actions console

--- test_and_report.sh
+++ test_and_report.sh
@@ -2,22 +2,12 @@
 docker run \
     -v $(pwd)/tests:/etc/newman --network=cumulus-api_default \
     --entrypoint /bin/bash -t postman/newman:ubuntu \
-    -c "npm i -g newman-reporter-htmlextra;
-        newman run /etc/newman/cumulus-regression-admin.postman_collection.json \
-        --environment=/etc/newman/postman_environment.docker-compose.json \
-        --reporter-htmlextra-browserTitle 'Cumulus' \
-        --reporter-htmlextra-title 'Cumulus Regression Tests; Admin Account' \
-        --reporter-htmlextra-titleSize 4 \
-        -r htmlextra --reporter-htmlextra-export /etc/newman/cumulus-admin.html"
+    -c "newman run /etc/newman/cumulus-regression-admin.postman_collection.json \
+        --environment=/etc/newman/postman_environment.docker-compose.json"
 
 # Run Tests For Non-Admin User
 docker run \
     -v $(pwd)/tests:/etc/newman --network=cumulus-api_default \
     --entrypoint /bin/bash -t postman/newman:ubuntu \
-    -c "npm i -g newman-reporter-htmlextra;
-        newman run /etc/newman/cumulus-regression-user.postman_collection.json \
-        --environment=/etc/newman/postman_environment.docker-compose.json \
-        --reporter-htmlextra-browserTitle 'Cumulus' \
-        --reporter-htmlextra-title 'Cumulus Regression Tests; User Account' \
-        --reporter-htmlextra-titleSize 4 \
-        -r htmlextra --reporter-htmlextra-export /etc/newman/cumulus-user.html"
+    -c "newman run /etc/newman/cumulus-regression-user.postman_collection.json \
+        --environment=/etc/newman/postman_environment.docker-compose.json"

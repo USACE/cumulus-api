8f73d542e71690b8487df886b8c1e2d0e2c9605c
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2021-11-01 10:40:59 -0400

    replace long path with variable

--- utils/db_export_import/app/scripts/export.sh
+++ utils/db_export_import/app/scripts/export.sh
@@ -8,6 +8,6 @@ printf "\n1) Running pg_dump, saving to local storage (export.dump).\n"
 pg_dump --format=c --compress 5 --username $PGUSER --schema $SCHEMA --data-only > /app/data/export.dump
 
 printf "\n2) Copying export.dump to $S3_PATH/export.dump.\n\n"
-aws --endpoint-url $AWS_S3_ENDPOINT s3 cp /app/data/export.dump s3://${AWS_S3_BUCKET}/${CUMULUS_S3_DB_EXPORT_DIR}/${today}/export.dump
+aws --endpoint-url $AWS_S3_ENDPOINT s3 cp /app/data/export.dump s3://$S3_PATH/export.dump
 
 printf "\nDONE.\n\n"

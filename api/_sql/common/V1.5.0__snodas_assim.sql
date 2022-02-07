a2fa614a91b379bb965803b49f5ad9ed0e408e2e
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-02-07 16:15:01 -0500

    rename FPart

--- api/_sql/common/V1.5.0__snodas_assim.sql
+++ api/_sql/common/V1.5.0__snodas_assim.sql
@@ -4,4 +4,4 @@ INSERT INTO acquirable (id, name, slug) VALUES
 
 -- product
 INSERT INTO cumulus.product (id,slug,"label",temporal_duration,temporal_resolution,dss_fpart,parameter_id,description,unit_id,deleted,suite_id) VALUES
-    ('75f8cd8c-c0f5-4ee5-9397-4866ad9afcac','nohrsc-snodas-swe-corrections','Corrections',0,86400,'SNODAS','683a55b9-4a94-46b5-9f47-26e66f3037a8','SWE Nudging Field Rasters','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'c133e9e7-ddc8-4a98-82d7-880d5db35060');
+    ('75f8cd8c-c0f5-4ee5-9397-4866ad9afcac','nohrsc-snodas-swe-corrections','Corrections',0,86400,'SNODAS-CORRECTIONS','683a55b9-4a94-46b5-9f47-26e66f3037a8','SWE Nudging Field Rasters','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'c133e9e7-ddc8-4a98-82d7-880d5db35060');

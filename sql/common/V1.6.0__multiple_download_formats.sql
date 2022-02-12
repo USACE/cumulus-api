6ef43c02a519d940e7cf67c14f6b0df7dfdc327b
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2022-02-11 20:47:43 -0600

    hotfix; DEFAULT download_format_id for existing download rows

--- sql/common/V1.6.0__multiple_download_formats.sql
+++ sql/common/V1.6.0__multiple_download_formats.sql
@@ -10,4 +10,8 @@ INSERT INTO download_format (id, abbreviation, name, extension) VALUES
     ('215b13c1-f710-4688-a8ac-5a9e7d991bb0','tgz-cog','Gzip Compressed Tape Archive (tar)','tar.gz'),
     ('358d3ee2-dfdc-47ae-a104-b98613e6578b','dss7','HEC-DSS Version 7', 'dss');
 
-ALTER TABLE download ADD COLUMN download_format_id UUID NOT NULL REFERENCES download_format(id);
+-- https://stackoverflow.com/questions/512451/how-can-i-add-a-column-that-doesnt-allow-nulls-in-a-postgresql-database
+-- Set download_format_id corresponding to 'dss7' for all existing downloads in the database
+ALTER TABLE download ADD COLUMN download_format_id UUID NOT NULL DEFAULT '358d3ee2-dfdc-47ae-a104-b98613e6578b' REFERENCES download_format(id);
+-- Drop Default
+ALTER TABLE download ALTER COLUMN download_format_id DROP DEFAULT;

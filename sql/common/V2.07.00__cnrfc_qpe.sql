9efca204e02e82e632ce7c88a6b564038e63544f
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-08-12 16:39:13 -0500

    Update slug cnrfc-qpe to cnrfc-qpe-06h

--- sql/common/V2.07.00__cnrfc_qpe.sql
+++ sql/common/V2.07.00__cnrfc_qpe.sql
@@ -8,7 +8,7 @@ INSERT INTO suite (id, name, slug, description) VALUES
 
 -- add product
 INSERT INTO product (id,slug,"label",temporal_duration,temporal_resolution,dss_fpart,parameter_id,description,unit_id,deleted,suite_id) VALUES
-    ('eac3976f-6503-442d-bf20-1a2ffe555162','cnrfc-qpe','QPE',21600,21600,'CNRFC-QPE','eb82d661-afe6-436a-b0df-2ab0b478a1af','CNRFC Quantitative Precipitation Estimate (QPE)','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'469552d5-51f8-40d3-b1f4-658af894c3e8');
+    ('eac3976f-6503-442d-bf20-1a2ffe555162','cnrfc-qpe-06h','QPE',21600,21600,'CNRFC-QPE','eb82d661-afe6-436a-b0df-2ab0b478a1af','CNRFC Quantitative Precipitation Estimate (QPE)','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'469552d5-51f8-40d3-b1f4-658af894c3e8');
 
 -- product_tags
 INSERT INTO product_tags (product_id, tag_id) VALUES

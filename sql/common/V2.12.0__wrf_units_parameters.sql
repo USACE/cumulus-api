9c77ba6e7ef6312f520148cbac8c24635fa57e84
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-09-07 16:50:35 -0500

    Remove DELETEs from migrations

--- sql/common/V2.12.0__wrf_units_parameters.sql
+++ sql/common/V2.12.0__wrf_units_parameters.sql
@@ -1,10 +1,20 @@
 -- Insert new parameters
 INSERT INTO parameter (id, name) VALUES
-    ('f1d2fee8-ac86-499f-83be-b15bcde43d76','PRESSURE'),
-    ('a4cbef84-fd47-415a-910c-d143dcb20ba4','VELOCITY'),
-    ('7586a687-7f31-4fa0-8447-eb31d199a05a','IRRADIANCE'),
-    ('8dd6420d-e624-45c0-90ed-b5ec3756d2f1','HUMIDITY'),
-    ('256e12c8-93d4-4a79-b3ac-6259426a189a', 'TEMPERATURE');
+    ('c680d0a3-909e-4046-bbae-18127ad589a6','PRESSURE'),
+    ('90cf9fe0-ded9-45a8-abc6-78aa25f3fde3','PRESSURE-VAPOR'),
+    ('8328aa49-2c7d-454c-b7e7-d9b2ffeba728','PRESSURE-SURFACE'),
+    ('90208e7b-bc6f-47dd-a704-0be2423f9222','VELOCITY'),
+    ('c563cb29-b7b9-4f73-b1c3-a87d931ff5e0','VELOCITY-V10METER'),
+    ('2d03d408-dd0e-4b6e-8944-e453c63222a7','VELOCITY-U10METER'),
+    ('99e72720-70d2-4d56-aa0e-4f5bcbabe6d2','IRRADIANCE'),
+    ('14dc8407-f63a-4237-8251-5ef1746c3d89','IRRADIANCE-SHORTWAVE'),
+    ('6f3efef3-6d6c-4378-94e3-0d7be7321707','IRRADIANCE-LONGWAVE'),
+    ('8f616a56-63dc-45fd-854a-67abd21f7f2f','HUMIDITY'),
+    ('c2c25499-1bc0-4c92-b429-446eaaf37768','HUMIDITY-RELATIVE'),
+    ('5c993bde-6024-45ac-9b54-d4b87fac3b50','TEMPERATURE'),
+    ('ca11ed97-02b9-4a44-9280-70c08bc0d5f9','TEMPERATURE-GROUND'),
+    ('1997ea14-9908-46e5-8d59-0ec7b1b7fc5d','TEMPERATURE-DEWPOINT'),
+    ('37281336-ac37-4824-b83f-bcbc37a64daf','AIRTEMP-2METER');
 
 -- Insert new units
 INSERT INTO unit (id, name, abbreviation) VALUES
--- sql/common/V2.13.0__wrf_columbia_products.sql
+++ sql/common/V2.13.0__wrf_columbia_products.sql
@@ -1,45 +1,20 @@
--- rename acquirable
-UPDATE acquirable SET
-    name = 'wrf-columbia',
-    slug = 'wrf-columbia'
-    WHERE id = 'ec926de8-6872-4d2b-b7ce-6002221babcd';
-
--- delete acquirablefile
-DELETE FROM acquirablefile WHERE acquirable_id = 'ec926de8-6872-4d2b-b7ce-6002221babcd';
-
--- delete productfile
-DELETE FROM productfile WHERE product_id = (SELECT id FROM product WHERE slug = 'wrf-columbia-precip');
-
--- delete download_product
-DELETE FROM download_product WHERE product_id = (SELECT id FROM product WHERE slug = 'wrf-columbia-precip');
+-- insert acquirable
+INSERT INTO acquirable(id, name, slug) VALUES
+    ('e8ce6e5c-1eb8-459e-9da7-5e9e43006c47', 'wrf-columbia', 'wrf-columbia');
 
 -- insert new products
 INSERT INTO product (id,slug,"label",temporal_duration,temporal_resolution,dss_datatype_id, dss_fpart,parameter_id,description,unit_id,deleted,suite_id) VALUES
-    ('ff23d2b2-9481-4ba5-9df0-0512a2da774c','wrf-columbia-groundt','Surface Skin Temperature [F]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Surface Skin Temperature','256e12c8-93d4-4a79-b3ac-6259426a189a','Surface skin temperature (GROUND_T)','0c8dcd1f-93db-4e64-be1d-47b3462deb2a',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('4c30740c-7729-485f-932f-94938cd90770','wrf-columbia-u10','U wind at 10m [m/s]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'U wind at 10m','a4cbef84-fd47-415a-910c-d143dcb20ba4','U (horizontal component towards east) wind velocity at 10 m (U10_____)','d5e7b710-f759-4eef-a0eb-d3148d81c43c',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('665d13d1-f894-4297-9442-8351950ccddb','wrf-columbia-v10','V wind at 10m [m/s]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'V wind at 10m','a4cbef84-fd47-415a-910c-d143dcb20ba4','V (horizontal component towards north) wind velocity at 10 m (V10_____)','d5e7b710-f759-4eef-a0eb-d3148d81c43c',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('dd64968b-fb65-448d-a418-3bfa44ed51f8','wrf-columbia-pstarcrs','Surface Pressure [hPa]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Surface','f1d2fee8-ac86-499f-83be-b15bcde43d76','Surface pressure (PSTARCRS)','c32186a9-28c8-4fda-84cf-a107d4fd40b4',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('5fb03bff-126b-4cc9-8162-cb054c757487','wrf-columbia-vaporps','Vapor Pressure [hPa]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Vapor','f1d2fee8-ac86-499f-83be-b15bcde43d76','Vapor pressure (VAPOR_PS)','c32186a9-28c8-4fda-84cf-a107d4fd40b4',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('962ec366-e022-45d4-9528-f995b53c29e1','wrf-columbia-dewpntt','Dew Point Temperature [F]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Dew point','256e12c8-93d4-4a79-b3ac-6259426a189a','Dew point temperature (DEWPNT_T)','0c8dcd1f-93db-4e64-be1d-47b3462deb2a',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('fa6d9ae6-f307-4330-8e5c-24e10b137c22','wrf-columbia-rh','Relative Humidity [%]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Relative Humidity','8dd6420d-e624-45c0-90ed-b5ec3756d2f1','Relative humidity (RH______)','5dd42877-0967-432e-9d84-a0b7239b4647',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('6f138d28-b398-455a-8465-46e2ce1fc302','wrf-columbia-swdown','Shorwave Radiation [W/m2]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Shortwave','7586a687-7f31-4fa0-8447-eb31d199a05a','Shortwave radiation (SWDOWN__)','880d70f4-d17a-44ba-8f1c-8ff58a87ef89',false,'894205d5-cc55-4071-946b-d4027004cb40'),
-    ('2e3786cd-ea7e-46c4-8156-bc86abe7104b','wrf-columbia-lwdown','Longwave Radiation [W/m2]',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'Longwave','7586a687-7f31-4fa0-8447-eb31d199a05a','Longwave radiation (LWDOWN__)','880d70f4-d17a-44ba-8f1c-8ff58a87ef89',false,'894205d5-cc55-4071-946b-d4027004cb40');
-
--- update Columbia Precip
-UPDATE product SET
-    slug = 'wrf-columbia-precipah',
-    label = 'Precipitation [mm/hr]',
-    description = 'Precipitation (PRECIPAH)',
-    dss_fpart = 'Precipitation (PRECIPAH)'
-    WHERE slug = 'wrf-columbia-precip';
-
--- update Columbia Temperature at 2m
-UPDATE product SET
-    slug = 'wrf-columbia-t2',
-    label = 'Temperature at 2m [F]',
-    description = 'Air temperature at 2m (t2)',
-    dss_fpart = 'Air Temp at 2m'
-    WHERE slug = 'wrf-columbia-airtemp';
+    ('5b6ea634-7a59-41be-ae56-a38a5e7e1632','wrf-columbia-precipah','',3600,3600, '392f8984-2e4e-47ea-ae24-dad81d87f662', 'wrf-columbia','eb82d661-afe6-436a-b0df-2ab0b478a1af','Precipitation','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('97e1e3a5-6791-411a-b4b4-37ec16398e3b','wrf-columbia-t2','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','37281336-ac37-4824-b83f-bcbc37a64daf','Air temperature at 2 meter','0c8dcd1f-93db-4e64-be1d-47b3462deb2a',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('ff23d2b2-9481-4ba5-9df0-0512a2da774c','wrf-columbia-groundt','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','ca11ed97-02b9-4a44-9280-70c08bc0d5f9','Surface skin temperature (GROUND_T)','0c8dcd1f-93db-4e64-be1d-47b3462deb2a',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('4c30740c-7729-485f-932f-94938cd90770','wrf-columbia-u10','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','2d03d408-dd0e-4b6e-8944-e453c63222a7','U (horizontal component towards east) wind velocity at 10 m (U10_____)','d5e7b710-f759-4eef-a0eb-d3148d81c43c',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('665d13d1-f894-4297-9442-8351950ccddb','wrf-columbia-v10','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','c563cb29-b7b9-4f73-b1c3-a87d931ff5e0','V (horizontal component towards north) wind velocity at 10 m (V10_____)','d5e7b710-f759-4eef-a0eb-d3148d81c43c',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('dd64968b-fb65-448d-a418-3bfa44ed51f8','wrf-columbia-pstarcrs','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','8328aa49-2c7d-454c-b7e7-d9b2ffeba728','Surface pressure (PSTARCRS)','c32186a9-28c8-4fda-84cf-a107d4fd40b4',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('5fb03bff-126b-4cc9-8162-cb054c757487','wrf-columbia-vaporps','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','90cf9fe0-ded9-45a8-abc6-78aa25f3fde3','Vapor pressure (VAPOR_PS)','c32186a9-28c8-4fda-84cf-a107d4fd40b4',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('962ec366-e022-45d4-9528-f995b53c29e1','wrf-columbia-dewpntt','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','1997ea14-9908-46e5-8d59-0ec7b1b7fc5d','Dew point temperature (DEWPNT_T)','0c8dcd1f-93db-4e64-be1d-47b3462deb2a',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('fa6d9ae6-f307-4330-8e5c-24e10b137c22','wrf-columbia-rh','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','c2c25499-1bc0-4c92-b429-446eaaf37768','Relative humidity (RH______)','5dd42877-0967-432e-9d84-a0b7239b4647',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('6f138d28-b398-455a-8465-46e2ce1fc302','wrf-columbia-swdown','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','14dc8407-f63a-4237-8251-5ef1746c3d89','Shortwave radiation (SWDOWN__)','880d70f4-d17a-44ba-8f1c-8ff58a87ef89',false,'894205d5-cc55-4071-946b-d4027004cb40'),
+    ('2e3786cd-ea7e-46c4-8156-bc86abe7104b','wrf-columbia-lwdown','',0,3600, 'b1433fa7-645f-4e3c-b560-29cba59e80c6', 'wrf-columbia','6f3efef3-6d6c-4378-94e3-0d7be7321707','Longwave radiation (LWDOWN__)','880d70f4-d17a-44ba-8f1c-8ff58a87ef89',false,'894205d5-cc55-4071-946b-d4027004cb40');
 
 -- WRF product tags
 INSERT INTO product_tags (product_id, tag_id) VALUES

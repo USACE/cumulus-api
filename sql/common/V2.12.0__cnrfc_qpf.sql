2d126750260b56cbdef2161fd5ae3809d7244181
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-12-08 16:45:16 -0500

    Feature/cnrfc qpf 06h (#385)
    * new product cnrfc-qpe-06h
    
    * adjust local service dependancies
    
    * add migration of cnrfc-qpf-06h
    
    * rename migration
    
    * resolve docker-compose conflicts
    
    * delete old qpf file causing conflicts
    
    * fix sql migration

---
+++ sql/common/V2.12.0__cnrfc_qpf.sql
@@ -0,0 +1,12 @@
+-- add acquirable
+INSERT INTO acquirable (id, name, slug) VALUES
+    ('c22785cd-400e-4664-aef8-426734825c2c', 'cnrfc-qpf-06h', 'cnrfc-qpf-06h');
+
+-- add product
+INSERT INTO product (id,slug,"label",temporal_duration,temporal_resolution,dss_fpart,parameter_id,description,unit_id,deleted,suite_id,acceptable_timedelta,dss_datatype_id) VALUES
+	 ('f6f25a98-1de8-4546-b5d6-e2e0ff761db3','cnrfc-qpf-06h','QPF',21600,21600,'CNRFC-QPF','eb82d661-afe6-436a-b0df-2ab0b478a1af','CNRFC Quantitative Precipitation Forecast (QPF)','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'469552d5-51f8-40d3-b1f4-658af894c3e8','7 hour','392f8984-2e4e-47ea-ae24-dad81d87f662');
+
+-- product_tags
+INSERT INTO product_tags (product_id, tag_id) VALUES
+    ('f6f25a98-1de8-4546-b5d6-e2e0ff761db3','726039da-2f21-4393-a15c-5f6e7ea41b1f'),
+    ('f6f25a98-1de8-4546-b5d6-e2e0ff761db3','cc93b3f9-fbe1-4b35-8f9c-2d1515961c6a');

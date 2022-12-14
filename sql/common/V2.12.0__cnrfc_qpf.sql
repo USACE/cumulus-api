fd3d3051fe3de1ee2f74b1d3cd9a5d28153cc98a
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-12-14 16:53:14 -0500

    fix cnrfc and nwrfc units; add inches unit (#391)

--- sql/common/V2.12.0__cnrfc_qpf.sql
+++ sql/common/V2.12.0__cnrfc_qpf.sql
@@ -3,5 +3,6 @@ INSERT INTO acquirable (id, name, slug) VALUES
     ('c22785cd-400e-4664-aef8-426734825c2c', 'cnrfc-qpf-06h', 'cnrfc-qpf-06h');
 
 -- add product
+-- note: units are INCHES
 INSERT INTO product (id,slug,"label",temporal_duration,temporal_resolution,dss_fpart,parameter_id,description,unit_id,deleted,suite_id,acceptable_timedelta,dss_datatype_id) VALUES
-	 ('f6f25a98-1de8-4546-b5d6-e2e0ff761db3','cnrfc-qpf-06h','QPF',21600,21600,'CNRFC-QPF','eb82d661-afe6-436a-b0df-2ab0b478a1af','CNRFC Quantitative Precipitation Forecast (QPF)','e245d39f-3209-4e58-bfb7-4eae94b3f8dd',false,'469552d5-51f8-40d3-b1f4-658af894c3e8','7 hour','392f8984-2e4e-47ea-ae24-dad81d87f662');
+	 ('f6f25a98-1de8-4546-b5d6-e2e0ff761db3','cnrfc-qpf-06h','QPF',21600,21600,'CNRFC-QPF','eb82d661-afe6-436a-b0df-2ab0b478a1af','CNRFC Quantitative Precipitation Forecast (QPF)','c5eba078-61d1-46a0-b2e6-b7d41ec9c924',false,'469552d5-51f8-40d3-b1f4-658af894c3e8','7 hour','392f8984-2e4e-47ea-ae24-dad81d87f662');

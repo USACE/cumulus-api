27fe221a0e6b565a0de81ddf2e95a2f068715d75
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-06-26 09:41:46 -0400

    Add archive tag to aorc-csu-precip

--- sql/common/V2.40.0__aorc_csu_precip.sql
+++ sql/common/V2.40.0__aorc_csu_precip.sql
@@ -9,3 +9,8 @@ INSERT INTO suite (id, name, slug, description) VALUES
 -- add product
 INSERT INTO product (id,slug,"label",temporal_duration,temporal_resolution,dss_fpart,parameter_id,description,unit_id,deleted,suite_id,acceptable_timedelta,dss_datatype_id) VALUES
     ('12180440-9ab6-4c6c-b790-4cea558e85a7', 'aorc-csu-precip', '', 3600, 3600, 'AORC', 'eb82d661-afe6-436a-b0df-2ab0b478a1af', 'The Analysis Of Record for Calibration (AORC) is a gridded record of near-surface weather conditions covering the continental United States and Alaska and their hydrologically contributing areas. This product contains AORC 800m hourly precipitation data archived by Colorado State University (CSU).', 'e245d39f-3209-4e58-bfb7-4eae94b3f8dd', false, 'b87f898e-184d-47e2-a550-b25c33ebc4e0', NULL, '392f8984-2e4e-47ea-ae24-dad81d87f662');
+
+-- add archive tag
+INSERT INTO product_tags (product_id, tag_id) VALUES
+    ('12180440-9ab6-4c6c-b790-4cea558e85a7','17308048-d207-43dd-b346-c9836073e911')
+ON CONFLICT ON CONSTRAINT unique_tag_product DO NOTHING;

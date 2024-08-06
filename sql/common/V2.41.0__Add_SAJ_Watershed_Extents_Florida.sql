58a3419f636f9ad0dc05e83de654e3e17050bef5
katfeingold <kathryn.a.feingold@usace.army.mil>
2024-08-06 14:17:30 -0400

    Added new SAJ Florida watershed

--- sql/common/V2.41.0__Add_SAJ_Watershed_Extents_Florida.sql
+++ sql/common/V2.41.0__Add_SAJ_Watershed_Extents_Florida.sql
@@ -15,9 +15,9 @@
 --xmax = 1671800
 --ymax = 978700
 --ymin = 340000
-
-
+-- 4142c26c-0407-41ad-b660-8657ddb2be69 - SAJ ID
+-- providede by Adam bfbd5b34-ff07-41d4-87cf-d67c80f98be1
 
 -- add new watershed
 INSERT INTO watershed (id, slug, "name", geometry, office_id, output_srid) VALUES
-	 ('bfbd5b34-ff07-41d4-87cf-d67c80f98be1','florida','Florida',ST_GeomFromText('POLYGON ((989500 978700, 1671800 978700, 1671800 340000, 989500 340000, 989500 978700))',5070),'4142c26c-0407-41ad-b660-8657ddb2be69', 5070);
+	 ('4142c26c-0407-41ad-b660-8657ddb2be69','florida','Florida',ST_GeomFromText('POLYGON ((989500 978700, 1671800 978700, 1671800 340000, 989500 340000, 989500 978700))',5070),'4142c26c-0407-41ad-b660-8657ddb2be69', 5070);

690499be46ed2cdd2f475be755548e5c51123d6a
katfeingold <kathryn.a.feingold@usace.army.mil>
2024-08-19 15:19:09 -0400

    Adjusted the extents, as previous were in feet not meters

--- sql/common/V2.45.0__NWP_update_Crooked_River_Watershed_extents.sql
+++ sql/common/V2.45.0__NWP_update_Crooked_River_Watershed_extents.sql
@@ -2,17 +2,19 @@
 -- xmin,ymax (top left), xmax ymax (top right), xmax ymin (bottom right), xmin ymin (bottom left), xmin ymax (top left again)
 
 --NWP - Crooked River
--- x_min  -6825762.86388243
--- x_max  -6100604.304031842
--- y_min  8258151.748351492
--- y_max  9081730.308212077
+--  x_min  -2080496.6819047283
+-- x_max  -1859467.910804729
+-- y_min  2517089.687076909
+-- y_max  2768116.934176909
+
+
 --Rounded:
--- x_min  -6825763
--- x_max  -6100605
--- y_min  8258152
--- y_max  9081731
+-- x_min  -2080497
+-- x_max  -1859468
+-- y_min 2517090
+-- y_max  2768117
 
 
 UPDATE watershed
-	SET geometry = ST_GeomFromText('POLYGON ((-7245681 5375027, -6825665 5375027, -6825665 5057730, -7245681 5057730, -7245681 5375027))',5070)
+	SET geometry = ST_GeomFromText('POLYGON ((-2080497 2768117, -1859468 2768117,-1859468 2517090, -2080497 2517090, -2080497 2768117))',5070)
 WHERE id = '070204a3-66d9-471c-bd6e-ab59ea0858bb';

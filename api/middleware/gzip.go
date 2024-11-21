9d724516f9be536f0936fd678fc3d4ecc16a8e57
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2024-11-21 16:15:20 -0500

    add pg_featureserv

--- api/middleware/gzip.go
+++ api/middleware/gzip.go
@@ -1,8 +1,19 @@
 package middleware
 
 import (
+	"strings"
+
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 // GZIP is ready-to-go GZIP middleware based on echo middleware
-var GZIP = middleware.GzipWithConfig(middleware.GzipConfig{Level: 5})
+// GZIP is ready-to-go GZIP middleware with a skipper to exclude certain routes
+var GZIP = middleware.GzipWithConfig(middleware.GzipConfig{
+	Level: 5,
+	Skipper: func(c echo.Context) bool {
+		// Skip GZIP compression for routes starting with /features
+		// as compression messes with pg_featureserv
+		return strings.Contains(c.Request().URL.Path, "/features")
+	},
+})

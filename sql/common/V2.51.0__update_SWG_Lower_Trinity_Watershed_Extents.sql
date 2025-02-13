716de4986a64f6f1e4ae655be5a7f58e9dd26138
msweier <mitchell.s.weier@usace.army.mil>
2025-02-13 15:04:52 -0600

    update eastern  extent

--- sql/common/V2.51.0__update_SWG_Lower_Trinity_Watershed_Extents.sql
+++ sql/common/V2.51.0__update_SWG_Lower_Trinity_Watershed_Extents.sql
@@ -8,10 +8,8 @@ UPDATE watershed
 	SET geometry = ST_GeomFromText(
         'POLYGON ((
     81520 859025, 
-    131785 859025, 
-    131785 731300, 
+    146000 859025, 
+    146000 731300, 
     81520 731300, 
     81520 859025))',5070)
 WHERE id = '59e880ca-f242-4f9a-b1f8-06424f8bb23f';
-
-

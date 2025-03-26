705bd8e845c8b6b7c39f3db17b39a277e6cc22a0
Jeff Gregory <jeffrey.s.gregory@me.com>
2025-03-26 14:21:48 -0500

    revert packager back to previous before refactor

--- async_packager/packager.py
+++ async_packager/packager.py
@@ -3,6 +3,7 @@
 import json
 import multiprocessing
 import os
+from pathlib import Path
 import shutil
 import traceback
 from collections import namedtuple
@@ -26,6 +27,7 @@ from cumulus_packager.configurations import (
     WRITE_TO_BUCKET,
 )
 from cumulus_packager.packager import handler
+from cumulus_packager.utils import capi
 from cumulus_packager.utils.boto import s3_upload_file
 
 this = os.path.basename(__file__)
@@ -65,9 +67,7 @@ def handle_message(message):
         if len(PayloadResp.contents) == 0:
             handler.update_status(download_id, handler.PACKAGE_STATUS["FAILED"], 0)
             # TODO: Add new package_status in database to represent EMPTY condition
-            logger.info(
-                f'Empty Contents: No products selected in the request for download ID "{download_id}"'
-            )
+            logger.info(f'Empty Contents: No products selected in the request for download ID "{download_id}"')
         else:
             package_file = handler.handle_message(PayloadResp, dst.name)
 
